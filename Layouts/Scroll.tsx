feb59ad1b13bac852df0f9beb65943db58032c63
frostzt <aidenfrostbite@gmail.com>
2021-06-01 21:23:05 +0530

    working on header

---
+++ Layouts/Scroll.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Scroll: React.FC = () => {
+  return <div></div>;
+};
+
+export default Scroll;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+
+// Styles
+import { motion } from "framer-motion";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <main className={styles.main}>
       <Head>
         <title>Sourav Rawat | Web Developer and Desingner</title>
         <meta
@@ -13,6 +16,20 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    </div>
+      <motion.header className={styles.header}>
+        <motion.div className={styles.greeting}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="/images/illustrations/hand.png"
+              className={styles.imageContainer__image}
+              priority={true}
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+        </motion.div>
+        <motion.div className={styles.name}></motion.div>
+      </motion.header>
+    </main>
   );
 }
