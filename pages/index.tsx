66b1c2d1bc3a832683c42005f8f1d55643007529
frostzt <aidenfrostbite@gmail.com>
2021-06-02 11:40:43 +0530

    working on header

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,13 @@ import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   const headerControl = useAnimation();
+  const greetingControl = useAnimation();
+  const nameControl = useAnimation();
 
   const sequence = async () => {
     await headerControl.start({ border: "6rem solid #fff" }, { duration: 1 });
+    await greetingControl.start({ opacity: 1, y: 0 }, { duration: 0.5 });
+    return await nameControl.start({ opacity: 1, y: 0 }, { duration: 0.5 });
   };
 
   useEffect(() => {
@@ -20,7 +24,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Head>
-        <title>Sourav Rawat | Web Developer and Desingner</title>
+        <title>Sourav Rawat | Web Developer and Designer</title>
         <meta
           name="description"
           content="Sourav Rawat's Portfolio, a web developer and designer"
@@ -34,7 +38,11 @@ export default function Home() {
         animate={headerControl}
         className={styles.header}
       >
-        <motion.div className={styles.greeting}>
+        <motion.div
+          initial={{ opacity: 0, y: 250 }}
+          animate={greetingControl}
+          className={styles.greeting}
+        >
           <div className={styles.imageContainer}>
             <Image
               src="/images/illustrations/hand.png"
@@ -46,7 +54,13 @@ export default function Home() {
           </div>
           <div className={styles.hi}>HI</div>
         </motion.div>
-        <motion.div className={styles.name}>I am Sourav!</motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: -100 }}
+          animate={nameControl}
+          className={styles.name}
+        >
+          I am Sourav!
+        </motion.div>
       </motion.header>
     </main>
   );
