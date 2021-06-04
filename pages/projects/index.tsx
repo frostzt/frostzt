04c6ea7a4340f6e75f7295ad577394984d563d5e
frostzt <aidenfrostbite@gmail.com>
2021-06-04 12:52:55 +0530

    creating the projects page

---
+++ pages/projects/index.tsx
@@ -0,0 +1,38 @@
+import Head from "next/head";
+
+// Styles and animations
+import { motion } from "framer-motion";
+import styles from "./index.module.scss";
+
+const Projects = () => {
+  return (
+    <motion.div className={styles.container}>
+      <Head>
+        <title>Projects</title>
+      </Head>
+      <motion.h2
+        initial={{
+          fontSize: "25rem",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+        animate={{
+          left: "-15%",
+          top: "35%",
+          fontSize: "22rem",
+          transform: "translate(-50%, -50%)",
+          rotateZ: "-90deg",
+        }}
+        transition={{ duration: 0.8 }}
+        className={styles.projectTitle}
+      >
+        projects
+      </motion.h2>
+      <motion.div className={styles.fixed}></motion.div>
+      <motion.div className={styles.projectsContainer}></motion.div>
+    </motion.div>
+  );
+};
+
+export default Projects;
