620487a1efc339f52c0ab0d87a8efe798bcc4e93
frostzt <aidenfrostbite@gmail.com>
2021-06-05 11:40:33 +0530

    implemented smooth and skew scrolling on projects page, also created a wrapping component to make it able to work anywhere

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -9,6 +9,9 @@ import styles from "./index.module.scss";
 // Components
 import Projects from "../../Components/Projects/Projects";
 
+// Layout
+import Scroll from "../../Layouts/Scroll";
+
 const ProjectsPage = () => {
   const [currentSelect, setCurrentSelect] = useState<string>("projects");
 
@@ -48,47 +51,49 @@ const ProjectsPage = () => {
   };
 
   return (
-    <motion.div className={styles.container}>
-      <Head>
-        <title>Projects</title>
-      </Head>
-      {/* Side Projects Heading */}
-      <motion.h2
-        initial="normal"
-        animate="animated"
-        variants={projectVariant}
-        onClick={() => setCurrentSelect("projects")}
-        transition={{ duration: 0.8 }}
-        className={cx([
-          styles.projectTitle,
-          currentSelect === "projects" ? styles.selected : "",
-        ])}
-      >
-        projects
-      </motion.h2>
-      <div className={styles.projectFixed}>&nbsp;</div>
+    <Scroll>
+      <motion.div className={styles.container}>
+        <Head>
+          <title>Projects</title>
+        </Head>
+        {/* Side Projects Heading */}
+        <motion.h2
+          initial="normal"
+          animate="animated"
+          variants={projectVariant}
+          onClick={() => setCurrentSelect("projects")}
+          transition={{ duration: 0.8 }}
+          className={cx([
+            styles.projectTitle,
+            currentSelect === "projects" ? styles.selected : "",
+          ])}
+        >
+          projects
+        </motion.h2>
+        <div className={styles.projectFixed}>&nbsp;</div>
 
-      {/* Main Content */}
-      <motion.div className={styles.projectsContainer}>
-        <Projects />
+        {/* Main Content */}
+        <motion.div className={styles.projectsContainer}>
+          <Projects />
+        </motion.div>
+
+        {/* Side Skill Heading */}
+        <div className={styles.skillFixed}>&nbsp;</div>
+        <motion.h2
+          initial="normal"
+          animate="animated"
+          variants={skillsVariant}
+          transition={{ duration: 0.8 }}
+          onClick={() => setCurrentSelect("skills")}
+          className={cx([
+            styles.projectTitle,
+            currentSelect === "skills" ? styles.selected : "",
+          ])}
+        >
+          skills
+        </motion.h2>
       </motion.div>
-
-      {/* Side Skill Heading */}
-      <div className={styles.skillFixed}>&nbsp;</div>
-      <motion.h2
-        initial="normal"
-        animate="animated"
-        variants={skillsVariant}
-        transition={{ duration: 0.8 }}
-        onClick={() => setCurrentSelect("skills")}
-        className={cx([
-          styles.projectTitle,
-          currentSelect === "skills" ? styles.selected : "",
-        ])}
-      >
-        skills
-      </motion.h2>
-    </motion.div>
+    </Scroll>
   );
 };
 
