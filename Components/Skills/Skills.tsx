d5217442f2f2f5020443d00a2533c5635af8ccdb
frostzt <aidenfrostbite@gmail.com>
2021-06-06 09:00:11 +0530

    working on developing skills component

--- Components/Skills/Skills.tsx
+++ Components/Skills/Skills.tsx
@@ -4,8 +4,16 @@ import React from "react";
 import { motion } from "framer-motion";
 import styles from "./Skills.module.scss";
 
-const Skills = () => {
-  return <div className={styles.container}></div>;
+const Skills: React.FC = () => {
+  return (
+    <motion.div className={styles.container}>
+      <motion.div className={styles.languages}></motion.div>
+      <motion.div className={styles.frontend}></motion.div>
+      <motion.div className={styles.backend}></motion.div>
+      <motion.div className={styles.databases}></motion.div>
+      <motion.div className={styles.tools}></motion.div>
+    </motion.div>
+  );
 };
 
 export default Skills;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,10 @@ function MyApp({ Component, pageProps }: AppProps) {
           href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.11.0/devicon.min.css"
+        />
       </Head>
       <Component {...pageProps} />
     </Fragment>
