975d9eb9df2b7b4089dfa53002f504df898a7caa
frostzt <aidenfrostbite@gmail.com>
2021-07-08 16:57:03 +0530

    created basic navigation, links to true destination yet to be inserted

--- Components/Header/Header.tsx
+++ Components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import cx from "classnames";
 import styles from "./Header.module.scss";
 import NavigationBar from "../NavigationBar/NavigationBar";
 
@@ -5,6 +6,12 @@ const Header = () => {
   return (
     <header className={styles.container}>
       <NavigationBar />
+      <div className={styles.content}>
+        <h1 className={cx([styles.title, "heading-primary"])}>
+          A Full-Stack Developer and Designer who loves to create things and
+          wants to have an impact on the world!
+        </h1>
+      </div>
     </header>
   );
 };
