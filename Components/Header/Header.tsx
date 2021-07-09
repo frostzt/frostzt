6a88190c4cfc464614801d3e6fe32f0a4938fea3
frostzt <aidenfrostbite@gmail.com>
2021-07-09 11:58:36 +0530

    working on header animation

--- Components/Header/Header.tsx
+++ Components/Header/Header.tsx
@@ -1,15 +1,39 @@
+import cx from "classnames";
+import { useEffect, useState } from "react";
 import styles from "./Header.module.scss";
 import NavigationBar from "../NavigationBar/NavigationBar";
 
 const Header = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (counter === 2) {
+        setCounter(0);
+      } else {
+        setCounter((counter) => counter + 1);
+      }
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  console.log(counter);
+
   return (
     <div className={styles.container}>
       <NavigationBar />
       <div className={styles.wrapper}>
         <h1 className={styles.heading}>
-          <span className={styles.heading__part}>DESIGN</span>
-          <span className={styles.heading__part}>DEVELOP</span>
-          <span className={styles.heading__part}>IMPROVE</span>
+          <span className={cx([styles.heading__part, counter === 0 ? styles.animate : null])}>
+            DESIGN
+          </span>
+          <span className={cx([styles.heading__part, counter === 1 ? styles.animate : null])}>
+            DEVELOP
+          </span>
+          <span className={cx([styles.heading__part, counter === 2 ? styles.animate : null])}>
+            IMPROVE
+          </span>
         </h1>
         <p className={styles.content}>Hi! I am Sourav.</p>
       </div>
