13b98ed802ee323407122c4fbcf87f5339668769
frostzt <aidenfrostbite@gmail.com>
2021-07-09 15:08:48 +0530

    Blog posts up and ready

--- Components/BlogStuff/BlogPost/BlogPost.tsx
+++ Components/BlogStuff/BlogPost/BlogPost.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
 import styles from "./BlogPost.module.scss";
 
-const BlogPost = () => {
-  return <div className={styles.post}></div>;
+interface Props {
+  title: string;
+  description: string;
+}
+
+const BlogPost: React.FC<Props> = ({ title, description }) => {
+  return (
+    <div className={styles.post}>
+      <h3 className={styles.title}>{title}</h3>
+      <p className={styles.description}>{description}</p>
+      <a className="make-special" href="https://frostzt.netlify.app/failure/">
+        Read more.
+      </a>
+    </div>
+  );
 };
 
 export default BlogPost;
--- Components/BlogStuff/BlogSnip/BlogSnip.tsx
+++ Components/BlogStuff/BlogSnip/BlogSnip.tsx
@@ -1,11 +1,17 @@
 import cx from "classnames";
 import styles from "./BlogSnip.module.scss";
+import BlogPost from "../BlogPost/BlogPost";
 
 const BlogSnip = () => {
   return (
     <div className={styles.container}>
       <h2 className={cx([styles.title, "heading-secondary", "make-special"])}>Philosophy?</h2>
-      <div className={styles.postsContainer}></div>
+      <div className={styles.postsContainer}>
+        <BlogPost
+          title="Failure, according to me"
+          description="Failure is a word I guess literally every person might fear in this world! Be it a man or woman, surprisingly even animals have a sense of it."
+        />
+      </div>
     </div>
   );
 };
