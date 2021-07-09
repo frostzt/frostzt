98c2d2bc3c060184d20308f0c790df662999bf1f
frostzt <aidenfrostbite@gmail.com>
2021-07-09 10:33:28 +0530

    changed the color theme and now improving the header

--- Components/Header/Header.tsx
+++ Components/Header/Header.tsx
@@ -6,10 +6,10 @@ const Header = () => {
     <div className={styles.container}>
       <NavigationBar />
       <div className={styles.wrapper}>
-        <h1>
-          <span>DESIGN</span>
-          <span>DEVELOP</span>
-          <span>IMPROVE</span>
+        <h1 className={styles.heading}>
+          <span className={styles.heading__part}>DESIGN</span>
+          <span className={styles.heading__part}>DEVELOP</span>
+          <span className={styles.heading__part}>IMPROVE</span>
         </h1>
         <p className={styles.content}>Hi! I am Sourav.</p>
       </div>
