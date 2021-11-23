9551a0c5c629aec09c2cf7d573aa2db55bee264a
frostzt <aidenfrostbite@gmail.com>
2021-11-23 13:50:03 +0530

    main page ready for mobile devices

---
+++ pages/projects/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next';
+import React from 'react';
+import Container from '../../Layout/Container/Container';
+
+const ProjectPage: NextPage = () => {
+  return (
+    <Container>
+      <h1>Projects</h1>
+    </Container>
+  );
+};
+
+export default ProjectPage;
